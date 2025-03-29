be427728a1a840975673fed875089eac5f6a5c81
Hannes Haimilahti <hhaimilahti@gmail.com>
2025-03-29 18:10:41 +0200

    Decode HTML entities from transcript

--- src/summarizerPopup.tsx
+++ src/summarizerPopup.tsx
@@ -1,4 +1,5 @@
-import { Backdrop, FormControl, IconButton, InputLabel, MenuItem, Paper, Select, TextField, Typography } from "@mui/material";
+import { Backdrop, Paper, Typography } from "@mui/material";
+import { decode, encode } from "html-entities";
 import { useEffect, useState } from "react";
 import { YoutubeTranscript } from "youtube-transcript";
 
@@ -7,27 +8,50 @@ interface Props {
     onDone: (summary: string) => void;
 }
 
+const States = {
+    GETTING_TRANSCRIPT: {
+        text: 'Getting transcript...',
+    },
+    SUMMARIZING: {
+        text: 'Summarizing...',
+    },
+    FINISHING: {
+        text: 'Finishing...',
+    },
+}
+
 export function SummarizerPopup({ url, onDone }: Props) {
     const [transcript, setTranscript] = useState('');
+    const [displayState, setDisplayState] = useState(States.GETTING_TRANSCRIPT);
 
-    YoutubeTranscript.fetchTranscript(url).then((transcript) => {
-        console.log(transcript);
-        setTranscript(transcript.map((t) => t.text).join(' '));
-    }).catch((err) => {
-        console.error(err);
-        onDone('Error fetching transcript');
-    });
+    useEffect(() => {
+        YoutubeTranscript.fetchTranscript(url).then((transcriptResponse) => {
+            let transcriptText = transcriptResponse.map((t) => t.text).join('\n');
+            // The transcript has HTML entities encoded twice like &amp;#39; -> &#39; -> '
+            transcriptText = decode(decode(transcriptText));
+            console.log("Transcript: ", transcriptText);
+            setTranscript(transcriptText);
+        }).catch((err) => {
+            console.error(err);
+            onDone('Error fetching transcript');
+        });
+    }, [url]);
 
     useEffect(() => {
         if (transcript == '') return;
+        setDisplayState(States.SUMMARIZING);
         console.log('Summarizing...');
+
+        setDisplayState(States.FINISHING);
         onDone('Summary of the video: ' + transcript);
     }, [transcript]);
 
     return (
         <Backdrop open={true} className="z-50">
             <Paper onClick={() => onDone('plah plah')} className="w-4/5 max-w-md p-4">
-                <Typography variant="h4" className="text-center">Summarizing...</Typography>
+                <Typography variant="h4" className="text-center">
+                    {displayState.text}
+                </Typography>
             </Paper>
         </Backdrop>
     );
