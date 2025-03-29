3ba35811419254cbe51744b28f45d934fe329d71
Hannes Haimilahti <hhaimilahti@gmail.com>
2025-03-29 16:51:05 +0200

    Fetch raw transcript

--- src/app.tsx
+++ src/app.tsx
@@ -5,8 +5,14 @@ import { createRoot } from 'react-dom/client';
 import { Settings } from './settings';
 import { SummarizerPopup } from './summarizerPopup';
 
+let initialSummaryText = "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+for (let i = 0; i < 10; i++) {
+    initialSummaryText = initialSummaryText.concat(initialSummaryText);
+}
+
 function App() {
-    const [summary, setSummary] = useState('Summaryyy');
+    const [url, setUrl] = useState('');
+    const [summary, setSummary] = useState(initialSummaryText);
     const [showSettings, setShowSettings] = useState(false);
     const [loading, setLoading] = useState(false);
 
@@ -19,16 +25,20 @@ function App() {
                 <Typography variant="h3" className="text-center">Youtube Video Summarizer</Typography>
                 <form>
                     <div className='flex flex-row items-center justify-center gap-2 items-stretch'>
-                        <TextField id="outlined-basic" label="Youtube URL" variant="outlined" style={{width: "25em"}} className="" />
+                        <TextField id="outlined-basic" label="Youtube URL" variant="outlined" onChange={(e) => setUrl(e.target.value)} style={{width: "25em"}} className="" />
                         <Button variant="contained" onClick={() => setLoading(true)}>Summarize</Button>
                     </div>
                 </form>
             </div>
-            <Paper elevation={4} className="p-4 m-4 grow">{summary}</Paper>
+            <Paper elevation={4} className="p-4 m-4 grow min-h-0">
+                <div className="overflow-y-auto h-full">
+                    {summary}
+                </div>
+            </Paper>
 
             {showSettings && <Settings onClose={() => setShowSettings(false)} />}
 
-            {loading && <SummarizerPopup onDone={(summary) => {
+            {loading && <SummarizerPopup url={url} onDone={(summary) => {
                 setSummary(summary);
                 setLoading(false);
             }} />}
--- src/summarizerPopup.tsx
+++ src/summarizerPopup.tsx
@@ -1,10 +1,29 @@
 import { Backdrop, FormControl, IconButton, InputLabel, MenuItem, Paper, Select, TextField, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import { YoutubeTranscript } from "youtube-transcript";
 
 interface Props {
+    url: string;
     onDone: (summary: string) => void;
 }
 
-export function SummarizerPopup({ onDone }: Props) {
+export function SummarizerPopup({ url, onDone }: Props) {
+    const [transcript, setTranscript] = useState('');
+
+    YoutubeTranscript.fetchTranscript(url).then((transcript) => {
+        console.log(transcript);
+        setTranscript(transcript.map((t) => t.text).join(' '));
+    }).catch((err) => {
+        console.error(err);
+        onDone('Error fetching transcript');
+    });
+
+    useEffect(() => {
+        if (transcript == '') return;
+        console.log('Summarizing...');
+        onDone('Summary of the video: ' + transcript);
+    }, [transcript]);
+
     return (
         <Backdrop open={true} className="z-50">
             <Paper onClick={() => onDone('plah plah')} className="w-4/5 max-w-md p-4">
